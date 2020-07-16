e0d43464af02ac665c07034440f6aca5ec0ad76d
Bruno Sesso <bruno.sesso@gmail.com>
2020-07-16 17:12:27 -0300

    Mutation - Save Client

--- src/main/java/com/udemy/compras/graphql/ClienteGraphQL.java
+++ src/main/java/com/udemy/compras/graphql/ClienteGraphQL.java
@@ -1,15 +1,17 @@
 package com.udemy.compras.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.udemy.compras.Cliente;
 import com.udemy.compras.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver{
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
 	@Autowired
 	private ClienteRepository clienteRep;
@@ -20,5 +22,10 @@ public class ClienteGraphQL implements GraphQLQueryResolver{
 
 	public List<Cliente> clientes() {
 		return clienteRep.findAll();
+	}
+
+	public Cliente saveCliente(Long id, String nome, String email){
+		Cliente c = new Cliente(id, nome, email);
+		return clienteRep.save(c);
 	}
 }
--- src/main/java/com/udemy/compras/graphql/QueryGraphQL.java
+++ src/main/java/com/udemy/compras/graphql/QueryGraphQL.java
@@ -1,10 +1,11 @@
 package com.udemy.compras.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
 @Component
-public class QueryGraphQL implements GraphQLQueryResolver{
+public class QueryGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
 	public String hello() {
 		return "Hello GraphQL";
