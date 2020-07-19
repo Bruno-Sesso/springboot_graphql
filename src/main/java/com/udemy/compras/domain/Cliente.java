b4742274bee47070421d579ab5cfab88f4f83c86
Bruno Sesso <bruno.sesso@gmail.com>
2020-07-19 12:25:56 -0300

    organizing from the domain, service and repository layers

--- src/main/java/com/udemy/compras/domain/Cliente.java
+++ src/main/java/com/udemy/compras/domain/Cliente.java
@@ -1,4 +1,4 @@
-package com.udemy.compras;
+package com.udemy.compras.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
---
+++ src/main/java/com/udemy/compras/service/ClienteService.java
@@ -0,0 +1,38 @@
+package com.udemy.compras.service;
+
+import com.udemy.compras.domain.Cliente;
+import com.udemy.compras.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository rep;
+
+    public Cliente findById(Long id){
+        return rep.findById(id).orElse(null);
+    }
+
+    public List<Cliente> findAll() {
+        return rep.findAll();
+    }
+
+    @Transactional
+    public Cliente save(Cliente c){
+        return rep.save(c);
+    }
+
+    @Transactional
+    public Boolean deleteById(Long id) {
+        if (rep.findById(id).isPresent()){
+            rep.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
